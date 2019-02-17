94d1305091779737ef752d74986ed123552ea26f
gouravkarwasara <gour6380@gmail.com>
2019-02-17 19:14:17 +0530

    Removed Key

--- app/src/main/java/com/gourav/news/network/NewsApi.kt
+++ app/src/main/java/com/gourav/news/network/NewsApi.kt
@@ -9,15 +9,15 @@ import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface NewsApi {
-
-    @Headers("X-Api-Key:6815924d54d640339217e09907d7a3b3")
+    //TODO add Your api key at the end of  X-Api-Key:
+    @Headers("X-Api-Key:")
     @GET("/v2/top-headlines")
     fun getHeadlines(
             @Query("category") category: String,
             @Query("country") country: String
     ): Call<ArticleResponseWrapper>
-
-    @Headers("X-Api-Key:6815924d54d640339217e09907d7a3b3")
+//TODO add Your api key at the end of  X-Api-Key:
+    @Headers("X-Api-Key:")
     @GET("/v2/sources")
     fun getSources(
             @Query("category") category: String,
